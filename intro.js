0e728a616edd55f0722b310e751a37c07b1b59b8
sjproctor <sjproctor950@gmail.com>
2021-04-24 12:54:14 -0700

    updates intro with new challenges

--- intro.js
+++ intro.js
@@ -1,51 +1,90 @@
 // Challenges
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
 
-// Arithmetic Challenge
-// Add 34 and 71
+// 1. Write the code that will log the outcome of 34 added to 71.
 console.log(34 + 71)
-// Subtract 67 from 123
+
+// 2. Write the code that will log the outcome of 67 subtracted from 123.
 console.log(123 - 67)
-// Multiply 56 and 23
+
+// 3. Write the code that will log the outcome of 56 multiplied by 23.
 console.log(56 * 23)
-// Divide 45 by 5
+
+// 4. Write the code that will log the outcome of 45 divided by 5.
 console.log(45 / 5)
-// Calculate 5 to the power of 7
+
+// 5. Write the code that will log the outcome of 5 to the power of 7.
 console.log(5 ** 7)
-// Find the remainder of 33/6
-console.log(33 % 6)
 
-// String Challenge
-// Find the length of a string containing your name
+// 6. Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 / 6)
+
+// 7. Write the code that will log the length of a string containing your name.
 console.log("Sarah".length)
-// Does your string include the letter "e"?
+
+// 8. Write the code that will log whether your string includes the letter "e"?
 console.log("Sarah".includes("e"))
+console.log("hello".includes("e"))
 
-// Boolean Challenge (return true or false)
-// Is 34 divided by 3 greater than 67 divided by 2?
-console.log(34/3 < 67/2)
-// Does 5 evaluate to the same as "5"?
+// 9. Write the code that will log the character at the first index of the string.
+console.log("Sarah".charAt(1))
+
+// 10. Write the code that will log the string in all uppercase letters.
+console.log("Sarah".toUpperCase())
+
+// Write the code that will log true or false for the following:
+// 11. Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
+
+// 12. Does 5 evaluate to the same as "5"?
 console.log(5 == "5")
-// Does 5 strictly equal "5"?
-console.log(5 === "5")
-// Does !3 strictly equal 3?
-console.log(!3 === 3)
-// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
-console.log("LEARN".length === 5 && "Student".length === 7)
-// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
-console.log("LEARN".length === 5 && "Student".length === 10)
 
-// Variables Challenge
-// Set a variable called favNum equal to your favorite number
-let favNum = 71
-// Log your favorite number divided by 2
-console.log(favNum / 2)
-// Set another variable called otherNum equal to 13
-let otherNum = 13
-// What is favNum divided by otherNum?
-console.log(favNum / otherNum)
-// Save a string as a variable
-let myString = "Hey there LEARN people!"
-// Find the length of the string
-console.log(myString.length)
-// Use the method charAt() to find what letter is at the 3rd index
-console.log(myString.charAt(3))
+// 13. Does 5 strictly equal "5"?
+console.log(5 === "5")
+
+// 14. Does !3 strictly equal 3?
+console.log(!3 === 3)
+
+// 15. Does `"LEARN".length` strictly equal 5 AND `"Student".length` strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
+
+// 16. Does `"LEARN".length` strictly equal 5 OR `"Student".length` strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10)
+
+// 17. Does "LEARN" contain the subset "RN"?
+console.log("LEARN".includes("RN"))
+
+// 18. Does "LEARN" contain the subset "rn"?
+console.log("LEARN".includes("rn"))
+
+// 19. Does "LEARN"[0] strictly equal "l"?
+console.log("LEARN"[0] === "l")
+
+// 20. Modify the code from the previous question to return `true`.
+console.log("LEARN"[0].toLowerCase() === "l")
+
+// Consider the variables:
+// ```javascript
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+// ```
+// 21. Write the code that will log `theAnswer` divided by 2.
+console.log(theAnswer / 2)
+
+// 22. Write the code that will log the length of `theQuestion`.
+console.log(theQuestion.length)
+
+// 23. Write the code that will log the index of the character `"f"` in the `theQuestion`.
+console.log(theQuestion.indexOf("f"))
+
+// 24. Write the code that will log the concatenation of the two variables.
+console.log(theQuestion + theAnswer)
+
+// 25. Write the code that will log `"the universe"`.
+console.log(theQuestion.slice(6, 18))
+
+// 26. Write the code that will log the character `"l"` from `theQuestion`.
+console.log(theQuestion[0])
+
+// 27. Write the code that will log whether `theQuestion.length` is greater that `theAnswer`.
+console.log(theQuestion.length > theAnswer)
